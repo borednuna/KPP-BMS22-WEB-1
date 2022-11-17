46e359b4340d6ac03712b24d402731ce8c37c628
borednuna <hanunshaka02@gmail.com>
2022-11-17 16:28:56 +0700

    finalize content

---
+++ src/fotografi/page_fotografi.js
@@ -0,0 +1,7 @@
+//  Scroll to top
+let arrow = document.getElementById("arrow");
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
